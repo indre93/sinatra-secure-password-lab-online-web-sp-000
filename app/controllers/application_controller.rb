f8094302cc884465cbae0d9d147b330256839ae5
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-27 18:43:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(username: params[:username], password: params[:password])
     binding.pry
-    if user.save && !user.nil? && !user.empty?
+    if user.save && !user[:username].nil? && !user[:username].empty?
       redirect "/login"
     else
       redirect "/failure"
